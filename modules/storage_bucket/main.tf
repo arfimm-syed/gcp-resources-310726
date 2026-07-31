b058c29afb1dd77cd802b6a5f334cffeca6e4cbe
Afzal <arfimm@gmail.com>
2026-07-31 13:12:00 +0100

    update3

--- modules/storage_bucket/main.tf
+++ modules/storage_bucket/main.tf
@@ -1,5 +1,6 @@
-resource "google_storage_bucket" "arfimm-bucket"{
+resource "google_storage_bucket" "my-bucket-arfimm" {
     for_each = var.storage_bucket
     name = each.value.name
     location = each.value.location
+    uniform_bucket_level_access = each.value.uniform_bucket_level_access
 }
--- modules/storage_bucket/variables.tf
+++ modules/storage_bucket/variables.tf
@@ -2,5 +2,6 @@ variable "storage_bucket"{
     type = map(object({
         name = string
         location = string
+        uniform_bucket_level_access = bool
     }))
 }
