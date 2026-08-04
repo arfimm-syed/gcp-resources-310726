fca3bffa434f89eef07936dff4ee4dd1456bef9b
Afzal <arfimm@gmail.com>
2026-08-04 12:23:17 +0100

    vm instance

--- env/main.tf
+++ env/main.tf
@@ -3,3 +3,9 @@ module "storage_bucket" {
   storage_bucket = var.storage_bucket
   project_id     = var.project_id
 }
+
+module "vm_instance" {
+  source         = "../modules/vm_instance"
+  my_vm_instance = var.my_vm_instance
+  project_id     = var.project_id
+}
---
+++ modules/vm_instance/main.tf
@@ -0,0 +1,18 @@
+resource "google_vm_instance" "my-vm-arfimm" {
+  for_each = var.vm_instance
+  name         = each.value.name
+  machine_type = each.value.machine_type
+  zone         = each.value.zone
+  project      = var.project_id
+
+  boot_disk {
+    initialize_params {
+      image = each.value.boot_disk_image
+    }
+  }
+
+  network_interface {
+    network = each.value.network
+    access_config {}
+  }
+}
